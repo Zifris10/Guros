e4289d92cc1a7380a55ca5a37858fc3c3694a9a0
Eduardo Martinez Torres <46201311+Zifris10@users.noreply.github.com>
2022-01-31 22:05:15 -0600

    sss

--- server/server.js
+++ server/server.js
@@ -16,9 +16,7 @@ app.set('views', '../views');
 
 app.set('view engine', 'pug');
 
-app.use(express.json({ limit: '50mb' }));
-
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 
 app.use(require('./routes/routes'));
 
