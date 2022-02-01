aefb30ac76a6e9cab06ddd0743494f26f1c7300d
Eduardo Martinez Torres <46201311+Zifris10@users.noreply.github.com>
2022-01-31 22:07:35 -0600

    nn

--- server/server.js
+++ server/server.js
@@ -6,21 +6,11 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const express = require('express');
 
-const path = require('path');
-
-const cors = require('cors');
-
 const app = express();
-
-app.set('views', '../views');
-
-app.set('view engine', 'pug');
 
 app.use(express.json());
 
 app.use(require('./routes/routes'));
-
-app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use('/api-documentation', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)));
 
